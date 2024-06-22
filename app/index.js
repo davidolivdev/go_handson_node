d18de0b25f698c6efe6029ebab96d718fab25045
David Felipe Santos Oliveira <david.oliveira@ciahering.com.br>
2024-06-22 12:32:00 -0300

    add arquviso de config

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 const port = 3000;
 
@@ -13,6 +15,7 @@ const config = {
 const connection = mysql.createConnection(config);
 
 connection.connect((err) => {
+  console.log(config.host);
   if (err) throw err;
   console.log('Connected to MySQL');
 });
